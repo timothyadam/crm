7dbef82226d15a39c43601e5889ada868c5c8cfe
wangxin <wangxindyouxiang@163.com>
2020-09-08 14:27:06 +0800

    权限相关更新

--- auth/src/main/java/com/timothy/auth/auth/AuthApplication.java
+++ auth/src/main/java/com/timothy/auth/auth/AuthApplication.java
@@ -2,8 +2,12 @@ package com.timothy.auth.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.timothy")
 public class AuthApplication {
 
     public static void main(String[] args) {
